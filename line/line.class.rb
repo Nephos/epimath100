58525e8d5abf6b5b64e3e1f89d38c8d1755160ce
poulet_a <poulet_a@epitech.eu>
2013-12-27 16:25:34 +0100

    fix line parametric

--- line/line.class.rb
+++ line/line.class.rb
@@ -5,17 +5,17 @@ require_relative '../point/point.class.rb'
 require_relative '../vector/vector.class.rb'
 
 class Line
-  attr_reader :point, :v_dir
+  attr_reader :point, :v_dir, :equ_para
 
   def initialize x, y, z, vx, vy, vz
     @point = Point.new x, y, z
     @v_dir = Vector.new vx, vy, vz
-    @equ_para = parametric @point, @v_dir
+    @equ_para = Line::parametric @point, @v_dir
   end
 
   def point_owned? p
     if p.is_a?Point
-      if parametric(p, @v_dir) == parametric(@point, @v_dir)
+      if Line::parametric(p, @v_dir) == Line::parametric(@point, @v_dir)
         true
       else
         false
@@ -32,15 +32,15 @@ class Line
       Error.call "Line::parametric : Invalid point"
     end
 
-    þ = 1 / @v_dir.x
-    { :x => @point.x + þ * @v_dir.x,
-      :y => @point.y + þ * @v_dir.y,
-      :z => @point.z + þ * @v_dir.z}
+    þ = 1 / v_dir.x
+    { :x => point.x + þ * v_dir.x,
+      :y => point.y + þ * v_dir.y,
+      :z => point.z + þ * v_dir.z}
   end
 
   def to_s(options={})
     if options == {}
-      "droite passant par le point (#{self.x};#{self.y};#{self.z}), de vecteur directeur #{@v_dir.to_s}"
+      "droite passant par le point (#{@point.x};#{@point.y};#{@point.z}), de vecteur directeur #{@v_dir.to_s}"
     else
       Error.call "Line::to_s : options is not valid"
     end
@@ -49,6 +49,3 @@ end
 
 line = Line.new 1, 2, 3, 1, 1, 1
 puts line
-puts line.x
-puts line.y
-puts line.z
