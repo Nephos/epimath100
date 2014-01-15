a4fd112eaab8ce04374b23d0f6c4846f35ad69ab
poulet_a <poulet_a@epitech.eu>
2014-01-15 21:37:00 +0100

    polynomial init

---
+++ polynominal/polynominal.class.rb
@@ -0,0 +1,59 @@
+#encoding: utf-8
+
+require_relative '../error/error.class'
+require_relative '../vector/vector.class'
+
+class Polynomial
+  EXPOSANT = {"0" => "⁰",
+              "1" => "¹",
+              "2" => "²",
+              "3" => "³",
+              "4" => "⁴",
+              "5" => "⁵",
+              "6" => "⁶",
+              "7" => "⁷",
+              "8" => "⁸",
+              "9" => "⁹"}
+
+  attr_accessor :coef
+
+  # Initialize the polynominal function
+  # Its coeficients are a, b, c, d ... with ax⁰ + bx¹ + cx² +dx³ ... = 0
+  # == Parameters:
+  # hash::
+  # hash is a hash which have several keys :a,:b,... which correspond to the coeficients
+  def initialize hash={}
+    Error.call "Polynomial::new : You hash is invalid" if !hash.is_a?Hash
+
+    @coef = hash.select{|coef,value| coef.match /[a-z]/}
+  end
+
+  def derive
+    dérivé = Polynomial.new
+
+    @coef.select{|coef,value| !coef.match /[a]/}.each do |coef,value|
+      dérivé_coef = :"#{(coef.to_s.ord - 1).chr}"
+      dérivé.coef[dérivé_coef] = value * (coef.to_s.ord - "a".to_s.ord)
+    end
+
+    return dérivé
+  end
+
+  def to_s
+    str = " = 0"
+    str = "#{@coef[:a]}" + str #if @coef[:a]
+
+    @coef.select{|coef,value| !coef.match /[a]/}.each do |coef,value|
+      #sign = "+"
+      #sign = "-" if value < 0
+      str = "#{value}x^#{(coef.to_s.ord - "a".ord)} + " + str #if value != 0
+    end
+
+    return str
+  end
+
+end
+
+pr = Polynomial.new({a:1, b:2, c:3, d:-12})
+puts pr
+puts pr.derive
