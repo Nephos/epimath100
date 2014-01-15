c2c87741498f085777b15bbda988b18ae31869f9
poulet_a <poulet_a@epitech.eu>
2014-01-15 20:16:38 +0100

    Fix matrix error => more lisibility

--- matrix/matrix.class.rb
+++ matrix/matrix.class.rb
@@ -4,19 +4,17 @@ require_relative '../error/error.class'
 require_relative '../vector/vector.class'
 
 class Matrix
-  attr_reader :columns, :lines, :v 
-  
+  attr_reader :columns, :lines, :v
+
   # Convert each element of an Array in an Integer => must be implented in Array Class
   def self.to_inttab arg, num=Float
-    if !arg.is_a?Array
-      Error.call "Matrix::to_inttab : '#{arg}' is not a valid Array", Error::ERR_HIGH
-    end
-    
+    Error.call "Matrix::to_inttab : '#{arg}' is not a valid Array", Error::ERR_HIGH if !arg.is_a?Array
+
     arg.size.times do |i|
-      # if it's an Array in an Array, the 
+      # if it's an Array in an Array, the
       if arg[i].is_a?Array
         arg[i] = self.to_inttab argv[i]
-      
+
       else
         if num == Integer
           arg[i] = arg[i].to_i
@@ -24,10 +22,10 @@ class Matrix
           arg[i] = arg[i].to_f
         end
       end
-      
+
     end
   end
-  
+
   # == Parameters:
   # tab::
   #   tab is a double Array like [ [1,2], [3,4], [1,4] ]. This array should be a valid Matrix tab or an other Matrix
@@ -41,14 +39,14 @@ class Matrix
 
     if tab.is_a?Array and tab.size > 0 and tab[0].is_a?Array
       @columns = tab[0].size
-      
+
       # check if the line have the good size
       tab.size.times do |i|
         @lines += 1
         if (tab[i].size != @columns)
           Error.call "Matrix::new : '#{tab[i]}' is not a valid line"
         end
-        
+
       end
     elsif tab.is_a?Matrix
       @v = tab.v
@@ -59,43 +57,41 @@ class Matrix
     end
     return
   end
-  
+
   def to_s
     out = ""
-    
+
     @v.each do |line|
       out << "["
-      
+
       # display all elements of this line
       line.each do |element|
         out << element.to_f.round(3).to_s << " "
       end
-    
+
       out << "\b]\n" # TODO : FIX THAT broggi_t
     end
     out
   end
-  
+
   def to_ary
     @v
   end
-  
+
   def to_vector
     return Vector.new self.get_val(0, 0), self.get_val(1, 0)
   end
-  
+
   def new_line tab=[]
-    if !tab.is_a?Array or tab.size != @column
-      Error.call "Matrix::new_line : Size of the new line (#{tab} => #{tab.size}) is not valid"
-    end
+    Error.call "Matrix::new_line : Size of the new line (#{tab} => #{tab.size}) is not valid" if !tab.is_a?Array or tab.size != @column
+
     @lines += 1
     @v << tab
   end
-  
+
   def new_column tab=[]
-    if !tab.is_a?Array or tab.size != @lines
-      Error.call "Matrix::new_column : Size of the new column (#{tab} => #{tab.size}) is not valid"
-    end
+    Error.call "Matrix::new_column : Size of the new column (#{tab} => #{tab.size}) is not valid" if !tab.is_a?Array or tab.size != @lines
+
     @columns += 1
     if tab.is_a? Array and tab.size == @lines
       @lines.times do |i|
@@ -107,18 +103,18 @@ class Matrix
       end
     end
   end
-  
+
   def del_line x=-1
     if x.is_a?Integer and @v[x] != nil
        @lines -= 1
        @v.delete_at x
-     else
+    else
        Error.call "Matrix::del_line : Line '#{x}' doesn't exist"
     end
   end
-  
+
   # == Parameters:
-  # x,y:: 
+  # x,y::
   #   Integers. They are the coordonates of the value which will extract from the matrix
   # == Returns:
   # a value fo the matrix
@@ -138,7 +134,7 @@ class Matrix
   end
 
   # == Parameters:
-  # x,y:: 
+  # x,y::
   #   Integers. They are the coordonates of the value which will write in the matrix
   # == Returns:
   # a value fo the matrix
@@ -160,45 +156,39 @@ class Matrix
       return @v[x][y]
     end
   end
-  
+
   # == Parameters:
-  # y:: 
+  # y::
   #   Integer. It's the n° line which is extracted
   # == Returns:
   # Array
   def get_line x
-    if !x.is_a?Integer or x < 0 or x >= @lines
-      Error.call "Matrix::get_line : Line #{x} doesn't exist"
-      return nil
-    end
-    
+    Error.call "Matrix::get_line : Line #{x} doesn't exist" if !x.is_a?Integer or x < 0 or x >= @lines
+
     return @v[x]
   end
-  
+
   # == Parameters:
-  # y:: 
+  # y::
   #   Integer. It's the n° column which is extracted
   # == Returns:
   # Array
   def get_column y
-    if !y.is_a?Integer or y < 0 or y >= @columns
-      Error.call "Matrix::get_column : Column #{y} doesn't exist"
-      return []
-    end
-    
+    Error.call "Matrix::get_column : Column #{y} doesn't exist" if !y.is_a?Integer or y < 0 or y >= @columns
+
     result = []
     @lines.times do |i|
       result << @v[i][y]
     end
     return result
   end
-  
+
   # == Params:
-  # matrix:: 
+  # matrix::
   #   matrix is a Matrix to compare.
   # == Returns:
   # True or False.
-  # == Usage:: 
+  # == Usage::
   # The function check if the current matrix and matrix:: have the same dimensions (linse and columns)
   def have_the_same_dimensions matrix
     if (matrix.is_a?Matrix and matrix.columns == @columns and matrix.lines == @lines)
@@ -207,26 +197,23 @@ class Matrix
       false
     end
   end
-  
+
   # == Parameters:
-  # t1,t2:: 
+  # t1,t2::
   #   Multiply each elements of t1 and t2 2b2 and sum all
-  # == Returns: 
+  # == Returns:
   # Float
   def self.mult_array(t1, t2)
-    if (!t1.is_a?Array or !t2.is_a?Array)
-      Error.call "Can't multiply this. One of the arguments is not an array.", Error::ERR_HIGH
-    elsif (t1.size != t2.size)
-      Error.call "Can't multiply this. Arrays do not have the same size.", Error::ERR_HIGH
-    end
-    
+    Error.call "Can't multiply this. One of the arguments is not an array.", Error::ERR_HIGH if (!t1.is_a?Array or !t2.is_a?Array)
+    Error.call "Can't multiply this. Arrays do not have the same size.", Error::ERR_HIGH if t1.size != t2.size
+
     result = 0.0
     t1.size.times do |i|
       result = (result + t1[i].to_f * t2[i].to_f).to_f
     end
     return result
   end
-  
+
   # == Parameters:
   # matrix::
   #   This argument is a Matrix or an Integer.
@@ -247,12 +234,10 @@ class Matrix
       Error.call "Matrix::* : Transformation implicite de Vector en Matrix", Error::ERR_LOW
       matrix = matrix.to_matrix
     end
-    
+
     if matrix.is_a?Matrix
-      if @columns != matrix.lines
-        Error.call "Matrix::* : Invalid multiplication at line #{matrix.lines} and column #{@columns}", Error::ERR_HIGH
-      end
-      
+      Error.call "Matrix::* : Invalid multiplication at line #{matrix.lines} and column #{@columns}", Error::ERR_HIGH if @columns != matrix.lines
+
       result = []
       @lines.times do |i|
         result << []
@@ -264,7 +249,7 @@ class Matrix
           result[y][x] = Matrix.mult_array(get_line(y), matrix.get_column(x))
         end
       end
-      
+
       return Matrix.new result
     #produit d'un entier et d'une matrix
     elsif matrix.is_a?Numeric
@@ -280,7 +265,7 @@ class Matrix
       Error.call "Matrix::* : Impossible de calculer cela (#{matrix} n'est pas une matrix)", Error::ERR_HIGH
     end
   end
-  
+
   # == Parameters:
   # matrix::
   #   This argument is a Matrix or an Integer. If it's a Matrix, it must have the same dimensions than the current Matrix.
@@ -307,17 +292,15 @@ class Matrix
     end
     Matrix.new result
   end
-  
+
   # == Returns::
   #   Numerical value which is the determinant of the matrix. It only work on 2x2
   def get_deter
-    if @columns != 2 or @lines != 2
-      Error.call "Matrix::get_deter : This error comes from get_deter which works only with 2x2 matrix"
-    end
-    
+    Error.call "Matrix::get_deter : This error comes from get_deter which works only with 2x2 matrix" if @columns != 2 or @lines != 2
+
     det = get_val(0, 0).to_i * get_val(1, 1).to_i
     det -= get_val(0, 1).to_i * get_val(1, 0).to_i
     return det
   end
-  
+
 end
