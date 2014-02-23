aa556f2767a6fb18bcfa5d35669c9968806ec8f9
poulet_a <poulet_a@epitech.eu>
2014-02-23 17:11:41 +0100

    Add tests

---
+++ test/test.rb
@@ -0,0 +1,16 @@
+#!/usr/bin/ruby
+#encoding: utf-8
+
+def test_put_header name=""
+  puts "=========="
+  puts "Begin Tests #{name}"
+  puts "=========="
+  puts
+end
+
+def test_put_footer name=""
+  puts "=========="
+  puts "End Tests #{name}"
+  puts "=========="
+  puts
+end
