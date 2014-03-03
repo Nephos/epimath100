86f12971c470381689a3814791c552bd5a843367
Arthur Poulet <poulet_a@epitech.eu>
2014-03-03 15:08:20 +0100

    Fix test, fix polynomial

--- lib/epimath100/polynomial.class.rb
+++ lib/epimath100/polynomial.class.rb
@@ -11,21 +11,23 @@ module EpiMath100
     # Initialize the polynominal function
     # Its coeficients are 1, 2, 3, 4 ... with '1'x⁰ + '2'x¹ + '3'x² ... = y
     # Each coeficients has an associated value (exemple : 2 => 1 correspond to 1x²)
+    # exemple :
+    # a = [] ; a[2] = 1 ; #correspond to 0 + 0x + 2x³
     #
     # == Parameters:
     # coef::
     #   coef is an array which have several keys 0, 1, 2,... which correspond to the coeficients
     # verb:: default is false
-    #   verb is true or false, or a integer. It will display more information if turned on when to_s.
-    #   if it's a integer, it must be in the list :
+    #   It will display more information if turned on when to_s.
+    #   It's a integer, it must be in the list :
     #   - 0 : returns ""
     #   - 1 : "y = equation"
-    #   - 2 : "f(x) = equation" (like true)
-    def initialize coef=[], verb=false
+    #   - 2 : "f(x) = equation" (default value)
+    def initialize coef=[], verb=0
       Error.call "Polynomial::new : Your coef is invalid" if !coef.is_a?Hash and !coef.is_a?Array
       coef = convert_hash(coef) if coef.is_a?Hash
       @coef = coef.select{|v| v.is_a?Numeric}
-      @verbose = verb
+      @verbose = 2
     end
 
     #calculate the derivated function of the current polynomial
@@ -33,10 +35,11 @@ module EpiMath100
     # Polynomial (the derivated function)
     def derive
       dérivé = Polynomial.new
-      return dérivé if @coef.size == 0
 
-      (1..(@coef.size)).times do |coef|
-        dérivé.coef[coef - 1] = dérivé[coef] * coef
+      if @coef.size > 0
+        (1..(@coef.size)).times do |coef|
+          dérivé.coef[coef - 1] = dérivé[coef] * coef
+        end
       end
 
       return dérivé
@@ -48,15 +51,16 @@ module EpiMath100
       str = ""
       str = "#{@coef[0].to_i}" + str #if @coef[:a]
 
-      return str if @coef.size == 0
-      (1..(@coef.size)).each do |coef|
-        #sign = "+"
-        #sign = "-" if value < 0
-        str = "#{@coef[coef]}x^#{coef} + " + str if @coef[coef].to_f != 0
+      if @coef.size > 0
+        (1..(@coef.size)).each do |coef|
+          #sign = "+"
+          #sign = "-" if value < 0
+          str = "#{@coef[coef]}x^#{coef} + " + str if @coef[coef].to_f != 0
+        end
       end
 
-      str = "f(x) = " + str if @verbose == true or @verbose == 2
-      str = "y = " + str if @verbose == 1
+      str = "f(x) = " + str     if @verbose == 2
+      str = "y = " + str        if @verbose == 1
 
       return str
     end
@@ -69,5 +73,6 @@ module EpiMath100
       end
       return y
     end
+
   end
 end
--- test/bin.rb
+++ test/bin.rb
@@ -4,16 +4,20 @@
 def test1
   require_relative '../lib/epimath100'
   puts  f = EpiMath100::Polynomial.new([1, 2])
+  puts  f.calc 0
+  puts  f.calc 1
+  puts  f.calc 3
+
   puts  g = EpiMath100::Rational.new([1, 2])
-  puts  f.calc 10
   puts  g.calc 10
 end
 
 def test2
   require "epimath100"
   puts  f = EpiMath100::Polynomial.new([1, 2])
-  puts  g = EpiMath100::Rational.new([1, 2])
   puts  f.calc 10
+
+  puts  g = EpiMath100::Rational.new([1, 2])
   puts  g.calc 10
 end
 
